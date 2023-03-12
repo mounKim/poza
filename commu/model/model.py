fb6b198580f92974ae26e80924b19d7961179106
mounKim <80572018+mounKim@users.noreply.github.com>
2023-03-12 16:07:55 +0900

    Update model.py

--- commu/model/model.py
+++ commu/model/model.py
@@ -626,7 +626,7 @@ class MemTransformerLM(nn.Module):
         self._create_params()
 
     def _create_params(self):
-        self.pos_emb = T5PositionalEmbedding(self.n_head * self.d_head)
+        self.pos_emb = PositionalEmbedding(self.d_model)
         self.r_w_bias = nn.Parameter(torch.Tensor(self.n_head, self.d_head))
         self.r_r_bias = nn.Parameter(torch.Tensor(self.n_head, self.d_head))
 
